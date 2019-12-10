ac64b3dd0c826318092487bb59b25a5b80d61df7
Jamie Haywood <36523771+JamieHollywood@users.noreply.github.com>
2019-12-10 17:22:05 +0000

    Test layout for the UI, the image probably wont work as its a hard-coded link. This will get changed to be part of a resource file when i work out how to do that

---
+++ mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QPixmap>
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPixmap pix("/Users/jamiehaywood/testAnnotation.jpg");
+    int w = ui->labelMainPic->width();
+    int h = ui->labelMainPic->height();
+    ui->labelMainPic->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
