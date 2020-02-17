eaf8b0788b0d96ab26e326e90d4e909750e8f20d
Ryan Ewart 2018 (N0727462) <n0727462@ntu.ac.uk>
2020-02-17 15:03:29 +0000

    Added some basic functionality for the buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QPixmap>
+#include <iostream>
+
+bool drawShape = false;
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -17,3 +21,16 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::mousePressEvent(QMouseEvent *event) {
+    if (drawShape == true){
+        std::cout<<"Test"<<std::endl;
+        drawShape = false;
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    drawShape = true;
+    std::cout<<"Button Test"<<std::endl;
+
+}
