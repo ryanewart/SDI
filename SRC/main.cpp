477e9be08f2e104260a061cf5fa9998747fff7e6
Edward Whale <ewhale@edwards-macbook.home>
2020-04-23 19:47:59 +0100

    Re-Added Main.cpp

---
+++ SRC/main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include "main.h"
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    //ptr = &w;
+    w.show();
+    return a.exec();
+    //Comment
+}
